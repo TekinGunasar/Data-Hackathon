d84a00541068b0897f7b5cbc40afc477c75bbd1d
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2022-04-09 16:47:22 -0700

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,10 +1,19 @@
 import pandas as pd
+from numpy import array
 
 class csv():
 
     def __init__(self,csv_file_path):
         self._csv = pd.read_csv(csv_file_path)
 
+    def get_proportion_label(self,label,value):
+        return sum(array(self._csv[label].values) == value)/len(self._csv[label].values)
+
+
 csv_file_path = 'advanced_trainset.csv'
 reader = csv(csv_file_path)
-print(reader.csv)
+
+label = 'Sentiment'
+value = 'negative'
+test = reader.get_proportion_label(label,value)
+print(test)
