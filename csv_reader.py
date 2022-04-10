fafe77fc2f3e6eb59a61eeff134a2d7eea99f4b2
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2022-04-09 17:09:15 -0700

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -3,8 +3,30 @@ from numpy import array
 
 class csv():
 
+    SENTENCE_INDEX = 'Sentence'
+
+
     def __init__(self,csv_file_path):
         self._csv = pd.read_csv(csv_file_path)
+
+
+    '''
+        Returns an individual entry from the csv
+    '''
+    def get_individual_entry(self,index):
+
+        sentences = self._csv[self.SENTENCE_INDEX].values
+        if index > len(sentences):
+            raise ValueError('Index entered exceeds maximum number of entries in csv')
+
+        return self._csv.iloc[index].values
+
+
+    '''
+        Removes the stop words from an individual sentence
+    '''
+    def remove_stop_word(self,sentence):
+        
 
     def get_proportion_label(self,label,value):
         return sum(array(self._csv[label].values) == value)/len(self._csv[label].values)
@@ -15,5 +37,7 @@ reader = csv(csv_file_path)
 
 label = 'Sentiment'
 value = 'negative'
-test = reader.get_proportion_label(label,value)
-print(test)
+
+index = 0
+sentence = reader.get_individual_entry(index)
+print(sentence)
