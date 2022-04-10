04c0e0c56419778f479063b5b26444d88ba06361
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2022-04-10 06:01:32 -0700

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -2,16 +2,27 @@ import pandas as pd
 from numpy import array
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-from IPython.display import display
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfTransformer,TfidfVectorizer
+
+ps = PorterStemmer()
 
 class csv():
 
     SENTENCE_INDEX = 'Sentence'
+    SENTIMENT_INDEX = 'Sentiment'
     STOP_WORDS = (' '.join(stopwords.words('english'))).lower().split(' ')
-    PUNKT = ['.',',','-','(',')']
+    PUNKT = ['.',',','-','(',')','$','\'s','n\'t','r\'e','\'d','%',':','``','&','\'','!','@']
+    word_encoder = []
+
+    bag_of_words_key = []
 
     def __init__(self,csv_file_path):
         self._csv = pd.read_csv(csv_file_path)
+
+    def get_target_variables(self):
+        return self._csv[self.SENTIMENT_INDEX].values
 
     '''
         Returns an individual entry from the csv
@@ -28,7 +39,7 @@ class csv():
         Removes the stop words from an individual sentence in the dataset
     '''
     def remove_stop_words(self,sentence):
-        sentence = sentence.split(' ')
+        sentence = word_tokenize(sentence)
         corrected_sentence = ' '.join(list(filter(lambda word: word.lower() not in self.STOP_WORDS and word not in self.PUNKT,
                                                   sentence)))
         return corrected_sentence
@@ -39,16 +50,50 @@ class csv():
     def remove_all_stop_words(self):
         self._csv[self.SENTENCE_INDEX] = self._csv[self.SENTENCE_INDEX].apply(self.remove_stop_words)
 
+    def stem_sentence(self,sentence):
+        sentence = word_tokenize(sentence)
+        stemmed_sentence = ' '.join([ps.stem(word) for word in sentence])
+        return stemmed_sentence
+
+    def stem_all_data(self):
+        self._csv[self.SENTENCE_INDEX] = self._csv[self.SENTENCE_INDEX].apply(self.stem_sentence)
+
+    def set_word_encoder(self,encoder):
+        self.word_encoder = encoder
+
+    def get_one_hot_encoding(self,sentence):
+        one_hot_vector = list(map(
+            lambda word:1 if word in sentence.split(' ') else 0,self.word_encoder
+        ))
+
+        return one_hot_vector
+
+    def bag_of_words(self):
+        self.remove_all_stop_words()
+        feature_vectors = self._csv[self.SENTENCE_INDEX].apply(self.get_one_hot_encoding).values
+
+        return list(feature_vectors)
+
     def get_proportion_label(self,label,value):
         return sum(array(self._csv[label].values) == value)/len(self._csv[label].values)
 
-csv_file_path = 'advanced_trainset.csv'
-reader = csv(csv_file_path)
 
-label = 'Sentiment'
-value = 'negative'
+    def tf_idf(self):
+        vectorizer=TfidfVectorizer(min_df=0.01)
+        self.remove_all_stop_words()
+        self.stem_all_data()
+        corpus = self._csv[self.SENTENCE_INDEX]
+        X = vectorizer.fit_transform(corpus)
+        feature_names = vectorizer.get_feature_names()
+        print(feature_names)
 
-index = 2
-reader.remove_all_stop_words()
-example_sentence = reader.get_individual_entry(index)
-print(example_sentence)
+
+path_to_csv = 'advanced_trainset.csv'
+reader = csv(path_to_csv)
+reader.tf_idf()
+
+
+
+
+
+
