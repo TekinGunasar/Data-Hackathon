60b60e1ac8223646a8b9d3aebb0f4daeac0134fb
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2022-04-10 06:02:11 -0700

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -17,3 +17,22 @@ class model_trainer():
 
         return _model.cv_results_[metrics_of_interest].sort_values('mean_test_score,ascending=False)
 
+from pandas import DataFrame
+from sklearn.utils import shuffle
+from sklearn.model_selection import GridSearchCV
+
+
+class model_trainer():
+
+    def __init__(self,models,training_set):
+        self.models = models
+        self.x = training_set[0]
+        self.y = training_set[1]
+
+    def get_grid_serach_results(self,model,parameter_grid,num_folds):
+        metrics_of_interest = ['param_n_estimators','param_max_depth','mean_test_score']
+        _model = GridSearchCV(model, parameter_grid, cv=num_folds, return_train_score=False, n_jobs=8)
+        _model.fit(self.x, self.y)
+
+        return _model.cv_results_[metrics_of_interest].sort_values('mean_test_score,ascending=False)
+
