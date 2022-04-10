4d5d99d6c45af2a3841f7ef28cf52f5bdb7cb737
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2022-04-09 17:55:24 -0700

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from numpy import array
+import nltk
+from nltk.corpus import stopwords
 
 class csv():
 
     SENTENCE_INDEX = 'Sentence'
-
+    STOP_WORDS = stopwords.words('english')
 
     def __init__(self,csv_file_path):
         self._csv = pd.read_csv(csv_file_path)
-
 
     '''
         Returns an individual entry from the csv
@@ -21,12 +22,19 @@ class csv():
 
         return self._csv.iloc[index].values
 
+    '''
+        Removes the stop words from an individual sentence in the dataset
+    '''
+    def remove_stop_words(self,sentence):
+        sentence = sentence.split(' ')
+        corrected_sentence = ' '.join(list(filter(lambda word: word not in self.STOP_WORDS,sentence)))
+        return corrected_sentence
 
     '''
-        Removes the stop words from an individual sentence
+        removes all stop words from the entire data set, replacing the sentences with their corrected version
     '''
-    def remove_stop_word(self,sentence):
-        
+    def remove_all_stop_words(self):
+        self._csv[self.SENTENCE_INDEX] = self._csv[self.SENTENCE_INDEX].apply(self.remove_stop_words)
 
     def get_proportion_label(self,label,value):
         return sum(array(self._csv[label].values) == value)/len(self._csv[label].values)
@@ -38,6 +46,7 @@ reader = csv(csv_file_path)
 label = 'Sentiment'
 value = 'negative'
 
-index = 0
-sentence = reader.get_individual_entry(index)
-print(sentence)
+index = 435
+reader.remove_all_stop_words()
+example_corrected_sentence = reader.get_individual_entry(index)
+print(example_corrected_sentence)
